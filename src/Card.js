83844de58e3af7d68448b3bf6abd64e914fb1d2f
tkdmadalin <moscaliuc.madalin@yahoo.com>
2022-06-15 10:23:45 +0300

    1

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Card = () => {
     return (
-<div>
-    <img src="https://robohash.org/sdas?800x200" alt="robot img"/>
+<div className="bg-light-green">
+    <img src="https://robohash.org/sdas?size=200x200" alt="robot img"/>
     <div>
         <h2>Jane Doe</h2>
         <p>jane.doe@gmail.com</p>
